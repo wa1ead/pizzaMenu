f469021dff7912a459e1b53f551f331bcce9057d
wa1ead <waliddraa2@gmail.com>
2023-11-15 17:08:07 +0100

    editing components

--- src/index.js
+++ src/index.js
@@ -49,10 +49,31 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're Currently Open</footer>
+  );
+  // return React.createElement("footer", null, "We're Currently Open");
 }
 
 function Pizza() {
